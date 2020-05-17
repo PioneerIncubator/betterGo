880654580771f145dc146ff10c80b57daf122386
YongHaoHu <christopherwuy@gmail.com>
2020-05-17 17:26:01 +0800

    Fix Enum.Map and add some tests.

--- enum/map.go
+++ enum/map.go
@@ -22,7 +22,6 @@ func InterfaceSlice(slice interface{}) []interface{} {
 }
 
 func Map(slice, anonymousFunc interface{}) {
-	// s := InterfaceSlice(listOfElements)
 	in := reflect.ValueOf(slice)
 	if in.Kind() != reflect.Slice {
 		panic("map: not slice")
@@ -46,49 +45,7 @@ func Map(slice, anonymousFunc interface{}) {
 	for i := 0; i < in.Len(); i++ {
 		ins[0] = in.Index(i)
 		tmp := fn.Call(ins[:])[0]
+		in.Index(i).Set(tmp)
 	}
-	// for i, v := range in {
-	// 	in[i] = fn.Call(v)[0]
-	// }
+	return
 }
-
-/*
-func Map(slice, anonymousFunc interface{}) []interface{} {
-	s := reflect.ValueOf(slice)
-	if s.Kind() != reflect.Slice {
-		panic("InterfaceSlice() given a non-slice type")
-	}
-
-	ret := make([]interface{}, s.Len())
-
-	for i := 0; i < s.Len(); i++ {
-		ret[i] = anonymousFunc(s.Index(i))
-	}
-
-	return ret
-}
-*/
-
-/*
-func InterfaceSlice(slice interface{}) []interface{} {
-	s := reflect.ValueOf(slice)
-	if s.Kind() != reflect.Slice {
-		panic("InterfaceSlice() given a non-slice type")
-	}
-
-	ret := make([]interface{}, s.Len())
-
-	for i := 0; i < s.Len(); i++ {
-		ret[i] = s.Index(i).Interface()
-	}
-
-	return ret
-}
-
-func Map(listOfElements interface{}, anonymousFunc func(element interface{}) interface{}) {
-	s := InterfaceSlice(listOfElements)
-	for i, v := range s {
-		s[i] = anonymousFunc(v)
-	}
-}
-*/
--- test/map.go
+++ test/map.go
@@ -6,55 +6,22 @@ import (
 	"github.com/YongHaoWu/betterGo/enum"
 )
 
-/*
-func double(a interface{}) interface{}{
-	in := reflect.ValueOf(a)
-	switch in.Kind() {
-	default:
-		return nil
-	case reflect.Int:
-		return a.(int) * 2
-	case reflect.Float64:
-		return a.(float64) * 2
-	}
-}
-*/
-
-func double(a int) int {
-	return a * 2
+func randomFn(origin int) int {
+	origin += 1
+	var b = 10
+	return origin * b
 }
 
-/*
-func MapADouble(argname_1 []int, argname_2 func(int ) int) {
-	lenSlice := len(argname_1)
-	if lenSlice == 0 {
-		return
-	}
-	for i := range argname_1 {
-		argname_1[i] = argname_2(argname_1[i])
-	}
+func varTestFn(origin int) int {
+	a := 10
+	return a * origin
 }
-*/
 
-func main() {
-	a := make([]int, 3)
-	for i := range a {
-		a[i] = i + 1
-	}
-
-	enum.Map(a, double)
-	//realOut := make([]int, len(out))
-	//for i := range out {
-	//	realOut[i] = out[i].(int)
-	//}
-	//for i := range a {
-	//	fmt.Println(a[i])
-	//}
-	expect := []int{2, 4, 6}
-
+func testMap(origin, expect []int, fn func(int) int) {
+	enum.Map(origin, fn)
 	flag := true
-	for i := range a {
-		if !(expect[i] == a[i]) {
+	for i := range origin {
+		if !(expect[i] == origin[i]) {
 			flag = false
 			break
 		}
@@ -62,6 +29,17 @@ func main() {
 	if flag {
 		fmt.Println("success, expect:", expect)
 	} else {
-		fmt.Printf("expected:%d, out:%d", expect, a)
+		fmt.Printf("expected:%d, out:%d", expect, origin)
 	}
+
+}
+
+func main() {
+	origin := []int{2, 4, 6}
+	expect := []int{30, 50, 70}
+	testMap(origin, expect, randomFn)
+
+	origin = []int{2}
+	expect = []int{20}
+	testMap(origin, expect, varTestFn)
 }
